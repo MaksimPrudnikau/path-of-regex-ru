0c74c2669efc16e7ae723836c37629c384cab322
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-12-30 23:41:02 +0300

    remove useless code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,8 @@ export const route = {
 
 export default function Home() {
   const user = createAsync(async () => getUser(), { deferStream: true });
+
+
   return (
     <main class="w-full p-4 space-y-2">
       <h2 class="font-bold text-3xl">Hello {user()?.username}</h2>
