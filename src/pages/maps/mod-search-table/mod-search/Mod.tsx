5cf9bdc4e03a710b8a19a021a8cc0d46cb19c3d4
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2026-01-04 16:55:32 +0300

    fix mod colors

--- src/pages/maps/mod-search-table/mod-search/Mod.tsx
+++ src/pages/maps/mod-search-table/mod-search/Mod.tsx
@@ -18,17 +18,18 @@ export function Mod({ mod, onClick, isSelected }: Props) {
       class={`btn text-start p-2 text-wrap justify-start h-fit`}
       classList={{
         "text-white": mod.rank === 0 && !isSelected(),
+        "text-black": mod.rank < 700 && isSelected(),
         "text-red-200": mod.rank > 0 && mod.rank <= 50 && !isSelected(),
         "text-red-300": mod.rank > 50 && mod.rank <= 150 && !isSelected(),
-        "text-red-400": mod.rank > 150 && !isSelected(),
+        "text-red-400": mod.rank > 150 && mod.rank < 700 && !isSelected(),
+
+        "bg-white": mod.rank === 0 && isSelected(),
+        "bg-red-200 text-black": mod.rank > 0 && mod.rank <= 50 && isSelected(),
+        "bg-red-300 text-black": mod.rank > 50 && mod.rank <= 150 && isSelected(),
+        "bg-red-400 text-black": mod.rank > 150 && mod.rank < 700 && isSelected(),
 
         "text-accent": mod.rank >= 700 && !isSelected(),
         "btn-accent": mod.rank >= 700 && isSelected(),
-
-        // Для selected состояний с низким рангом
-        "bg-red-200 text-black": mod.rank > 0 && mod.rank <= 50 && isSelected(),
-        "bg-red-300 text-black": mod.rank > 50 && mod.rank <= 150 && isSelected(),
-        "bg-red-400 text-black": mod.rank > 150 && isSelected(),
       }}
       onClick={handleClick}
       type="button"
