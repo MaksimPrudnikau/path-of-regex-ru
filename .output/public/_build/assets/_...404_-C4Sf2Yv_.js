437cfa7972ba0fcf4f89a6007380b8f5e3979eb6
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2026-01-10 18:40:44 +0300

    move build to docs

--- .output/public/_build/assets/_...404_-C4Sf2Yv_.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./web-yCR0qigV.js";var a=t('<main class="w-full p-4 space-y-2"><h1 class="font-bold text-xl">Page Not Found');function l(){return a()}export{l as default};
---
+++ docs/_build/assets/_...404_-C4Sf2Yv_.js
@@ -0,0 +1,9 @@
+import { t } from "docs/_build/assets/web-yCR0qigV.js";
+
+var a = t(
+  '<main class="w-full p-4 space-y-2"><h1 class="font-bold text-xl">Page Not Found',
+);
+function l() {
+  return a();
+}
+export { l as default };
