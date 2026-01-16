d47bec55f6890b44711abd8880c5f2d466aa5140
Prudnikau Maksim <soobadday@mail.ru>
2026-01-16 17:10:36 +0300

    fix button styles

--- src/pages/maps/header-with-profile/Header.tsx
+++ src/pages/maps/header-with-profile/Header.tsx
@@ -3,7 +3,7 @@ import { Profile } from "./Profile/Profile";
 export function HeaderWithProfile() {
   return (
     <div class={"w-full row justify-between"}>
-      <h2>Регулярное выражение для поиска карт:</h2>
+      <h2 class={"max-[550px]:hidden"}>Регулярное выражение для поиска карт:</h2>
       <Profile />
     </div>
   );
